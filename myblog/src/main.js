57a0b109f5ff576d31e82d1d810ed624025a93c0
peng <zhangpeng3710@gmail.com>
2023-02-08 15:33:02 +0800

    add router, vuex, vuetify dependencies

--- myblog/src/main.js
+++ myblog/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+Vue.config.productionTip = false
 
-createApp(App).mount('#app')
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')

0dbf93e84cb3daf0576ecc264383fe3b8721e6ff
peng <zhangpeng3710@gmail.com>
2023-02-08 19:55:10 +0800

    design AppBar

--- myblog/src/App.vue
+++ myblog/src/App.vue
@@ -1,55 +1,144 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+        app
+        color="primary"
+        dark
     >
+
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/logo.svg"
+            transition="scale-transition"
+            width="40"
         />
 
         <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+            alt="Vuetify Name"
+            class="shrink mt-1"
+            contain
+            min-width="100"
+            src="./assets/img.png"
+            width="100"
         />
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-tabs
+          v-model="tab"
+          centered
+          dark
+          icons-and-text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-tab @click="navigate2tab1">One</v-tab>
+        <v-tab @click="navigate2tab2">Two</v-tab>
+        <v-tab @click="navigate2tab3">Three</v-tab>
+        <v-tab @click="navigate2tab4">Four</v-tab>
+      </v-tabs>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
       </v-btn>
+
+      <MessageInAppBar></MessageInAppBar>
+
+      <AvatarInAppBar></AvatarInAppBar>
+
+
     </v-app-bar>
 
     <v-main>
       <router-view/>
     </v-main>
+
+    <v-footer
+        dark
+        padless
+    >
+      <v-card
+          flat
+          tile
+          class="indigo lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-4 white--text"
+              icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent
+          ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet
+          dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum
+          ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci
+          varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
+import AvatarInAppBar from "@/components/AvatarInAppBar";
+import MessageInAppBar from "@/components/MessageInAppBar";
 export default {
   name: 'App',
-
+  components: {MessageInAppBar, AvatarInAppBar},
   data: () => ({
-    //
+    user: {
+      initials: 'JD',
+      fullName: 'John Doe',
+      email: 'john.doe@doe.com',
+    },
+    selectedItem: 0,
+    items: [
+      {text: 'My Files', icon: 'mdi-folder'},
+      {text: 'Shared with me', icon: 'mdi-account-multiple'},
+      {text: 'Starred', icon: 'mdi-star'},
+      {text: 'Recent', icon: 'mdi-history'},
+      {text: 'Offline', icon: 'mdi-check-circle'},
+      {text: 'Uploads', icon: 'mdi-upload'},
+      {text: 'Backups', icon: 'mdi-cloud-upload'},
+    ],
+    icons: [
+      'mdi-facebook',
+      'mdi-twitter',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
+    right: null,
+    tab: null,
   }),
+
+  methods: {
+    navigate2tab1() {
+      this.$router.push({path: "/"});
+    },
+    navigate2tab2() {
+      this.$router.push({path: "/about"});
+    },
+    navigate2tab3() {
+      this.$router.push({path: "/about"});
+    },
+    navigate2tab4() {
+      this.$router.push({path: "/about"});
+    },
+  }
 };
 </script>
---
+++ myblog/src/components/MessageInAppBar.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-menu
+      bottom
+      min-width="200px"
+      offset-y
+      rounded
+  >
+
+    <template v-slot:activator="{ on }">
+      <v-badge
+          color="pink"
+          dot
+          offset-x="10"
+          offset-y="10"
+      >
+        <v-btn icon v-on="on">
+          <v-icon>mdi-message-processing</v-icon>
+        </v-btn>
+      </v-badge>
+    </template>
+
+
+    <v-list three-line width="500px">
+      <template v-for="(item, index) in items">
+        <v-subheader
+            v-if="item.header"
+            :key="item.header"
+            v-text="item.header"
+        ></v-subheader>
+
+        <v-divider
+            v-else-if="item.divider"
+            :key="index"
+            :inset="item.inset"
+        ></v-divider>
+
+        <v-list-item
+            v-else
+            :key="item.title"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+
+  </v-menu>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'MessageInAppBar',
+
+  data: () => ({
+
+    selectedItem: 0,
+    items: [
+      { header: 'Today' },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+        title: 'Brunch this weekend?',
+        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+        title: 'Oui oui',
+        subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+        title: 'Birthday gift',
+        subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+        title: 'Recipe to try',
+        subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+      },
+    ],
+
+  }),
+
+  methods: {}
+};
+</script>
